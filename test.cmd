87be53f01062ef35a6a0e90842eeee7e634197ef
JMDirksen <jeftadirksen@gmail.com>
2025-08-20 10:48:17 +0200

    Update test.cmd and test.sh to use consistent endpoint and improve expected output validation

--- test.cmd
+++ test.cmd
@@ -1,23 +1,33 @@
 @echo off
 
-curl -s -X PUT -d "key,value" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1">url.tmp
-curl -s -X PATCH -d "test,value1" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1">url.tmp
-curl -s -X PATCH -d "test,value2" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1">url.tmp
-curl -s -X PATCH -d "test,value3" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1">url.tmp
+curl -s -X PUT -d "key,value" "http://localhost:8080/test?maxlines=2&headerlines=1">url.tmp
+curl -s -X PATCH -d "A,value1" "http://localhost:8080/test?maxlines=2&headerlines=1">url.tmp
+curl -s -X PATCH -d "B,value2" "http://localhost:8080/test?maxlines=2&headerlines=1">url.tmp
+curl -s -X PATCH -d "C,value3" "http://localhost:8080/test?maxlines=2&headerlines=1">url.tmp
+curl -s -d "D,value4" "http://localhost:8080/test/">url.tmp
 
 set /p url=<url.tmp
 
 curl -s "%url%">output.tmp
 
-echo ======= This =======
-type output.tmp
-echo ====================
-echo.
-echo === Should equal ===
-echo key,value
-echo test,value2
-echo test,value3
-echo ====================
+echo key,value>expected.tmp
+echo B,value2>>expected.tmp
+echo C,value3>>expected.tmp
+echo D,value4>>expected.tmp
+
+fc output.tmp expected.tmp > nul
+if %errorlevel% neq 0 (
+    echo Output does not match expected values.
+    echo.
+    echo Expected:
+    type expected.tmp
+    echo.
+    echo Got:
+    type output.tmp
+) else (
+    echo OK
+)
 
 del url.tmp
 del output.tmp
+del expected.tmp
--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-url=$(curl -s -X PUT -d "key,value" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1")
-url=$(curl -s -X PATCH -d "test,value1" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1")
-url=$(curl -s -X PATCH -d "test,value2" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1")
-url=$(curl -s -X PATCH -d "test,value3" "http://localhost:8080/<mysecret>?maxlines=2&headerlines=1")
+url=$(curl -s -X PUT -d "key,value" "http://localhost:8080/test?maxlines=2&headerlines=1")
+url=$(curl -s -X PATCH -d "test,value1" "http://localhost:8080/test?maxlines=2&headerlines=1")
+url=$(curl -s -X PATCH -d "test,value2" "http://localhost:8080/test?maxlines=2&headerlines=1")
+url=$(curl -s -X PATCH -d "test,value3" "http://localhost:8080/test?maxlines=2&headerlines=1")
 
 output=$(curl -s -X GET "$url")
 
